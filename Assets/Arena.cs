101469b409b5018103dbb86637b7804c5baf368d
unknown <byrnec3@SA2502-PC02.win.rpi.edu>
2015-02-28 16:12:52 -0500

    Added arena script, enemy

---
+++ Assets/Arena.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arena : MonoBehaviour 
+{
+	public int arenaNumber;
+	public float spawnRate;
+
+	// awake is reserved for initalization of accessors
+	void Awake ()
+	{
+		Game.AddArena(this, arenaNumber);
+	}
+
+	// start contains initalization of things that are access dependent
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+	private new Transform transform;
+	private Rigidbody2D rb;
+	
+	public Player target;
+	public Arena arena;
+	
+	void Awake ()
+	{
+		transform = GetComponent<Transform>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Start () 
+	{
+		if(target == null)
+			setTarget( getArenaIndexFromPosition() );
+	}
+	
+	void Update () 
+	{
+		if(target == null)
+			setTarget( getArenaIndexFromPosition() );
+		
+		if(!target.knockedOut)
+			do 
+				continue;
+			while (false);
+	}
+	
+	public void setTarget(int arenaIndex)
+	{
+		target = Game.player[arenaIndex];
+		arena = Game.arena[arenaIndex];
+	}
+	
+	public int getArenaIndexFromPosition()
+	{
+		if(transform.position.x < 0)
+			return 0;
+		else
+			return 1;
+	}
+}

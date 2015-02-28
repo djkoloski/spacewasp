e1859aab0d267735d5aae7620459335fdd86d0d2
unknown <byrnec3@SA2502-PC02.win.rpi.edu>
2015-02-28 15:12:06 -0500

    Game contains constants for static reference

---
+++ Assets/Game.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Game {
+
+	// arena constants 
+	public static int screenHeight = 768;
+	public static int screenWidth = 1024;
+	public static int arenaHeight = 300;
+	public static int arenaWidth = 300;
+	public static int arenaBorderWidth = 30;
+
+	// arena reference
+	public static GameObject arena;
+
+	// player info 
+	public static PlayerInfo[] player; 
+
+	// enemy list
+	public static List<GameObject> enemy;
+
+	public class PlayerInfo 
+	{
+		public int killCount;
+		public int powerUp;//PowerUp
+		public GameObject gameObject;
+		public PlayerInfo(GameObject _gameObject)
+		{
+			gameObject  = _gameObject;
+		}
+	}
+}

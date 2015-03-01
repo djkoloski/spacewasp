759bf69be1244801f24eb5605adb3ff5f9fe5ffe
unknown <BYRNEC3@SA2502-PC02.win.rpi.edu>
2015-03-01 05:41:17 -0500

    Let's play the layers game

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -18,6 +18,11 @@ public class Enemy : MonoBehaviour
 	public float speed;
 	public bool type;
 	
+	public bool buffered;
+	public Vector3 bufferedDash;
+	
+	public GameObject hitbox;
+	private GameObject activeHitbox;
 	void Awake ()
 	{
 		transform = GetComponent<Transform>();
@@ -32,6 +37,7 @@ public class Enemy : MonoBehaviour
 		if(target == null)
 			SetTarget(GetArenaIndexFromPosition() );
 		Game.AddEnemy(gameObject, arena.arenaNumber);
+		buffered = false;
 	}
 	
 	void Update () 
@@ -49,7 +55,7 @@ public class Enemy : MonoBehaviour
 		if(attacking && !type)
 			animator.SetInteger("facing", -1);
 		if(!target.knockedOut && !attacking)
-			 PursueTarget();
+			PursueTarget();
 			 
 		transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
 	}
@@ -66,30 +72,88 @@ public class Enemy : MonoBehaviour
 		animator.SetBool("rest", false);
 	}
 	
+	public void PursueTarget()
+	{
+		if((target.transform.position - transform.position).magnitude > ( type ? 1.5 : .3) )
+		{
+			rb.velocity = speed * ((target.transform.position - transform.position).normalized) ;
+			animator.SetBool("walking", true);
+		}
+		else
+		{
+			rb.velocity = Vector2.zero;
+			Attack();
+		}
+		if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+			facing = (rb.velocity.x > 0 ? 0 : 2);
+		else
+			facing = (rb.velocity.y > 0 ? 1 : 3);
+		
+		if(!attacking)
+			animator.SetInteger("facing", facing);
+	}
+	
+	public void Attack()
+	{
+		attacking = true;
+		if(!type)
+		{
+			animator.SetBool("walking", false);
+			animator.SetInteger("facing", 1);
+		}	
+		else 
+		{
+			animator.SetBool("walking", false);
+			animator.SetInteger("facing", 1);
+		}
+	}
+	
 	public void ReAwake()
 	{
 		attacking = false;
+		buffered = false;
 	}
 	
 	public void DashStart()
 	{
-		
+		if(activeHitbox)
+			Destroy(activeHitbox);
+		activeHitbox = (GameObject)Instantiate(hitbox, transform.position, Quaternion.identity);
+		activeHitbox.transform.localScale = new Vector3(.8f, .8f);
+		activeHitbox.transform.parent = transform;
+		if(buffered)
+			rb.velocity = bufferedDash;
 	}
 	
 	public void DashEnd()
 	{
-		animator.SetBool("rest", true);
+		Destroy(activeHitbox);
+		buffered = true;
+		bufferedDash = rb.velocity;
+		rb.velocity = Vector2.zero;
+	}
+	
+	public void RestStart()
+	{
+		if(buffered)
+			rb.velocity = Vector2.zero;
+		buffered = false;
+	}
+	
+	public void SlideEnd()
+	{
 		rb.velocity = Vector2.zero;
 	}
 	
 	public void ExpolsionStart()
 	{
-		
+		activeHitbox = (GameObject)Instantiate(hitbox, transform.position, Quaternion.identity);
+		activeHitbox.transform.localScale = new Vector3(1.2f, 1.2f);
 	}
 	
 	public void ExpolsionEnd()
 	{
-		
+		Destroy(activeHitbox);
 	}
 	
 	public void End()
@@ -117,39 +181,17 @@ public class Enemy : MonoBehaviour
 			return 1;
 	}
 	
-	public void PursueTarget()
+	void OnTriggerEnter2D(Collider2D other)
 	{
-		if((target.transform.position - transform.position).magnitude > ( type ? 1.5 : .3) )
+		Debug.Log("hello");
+		if(other.gameObject == target.gameObject)
 		{
-			rb.velocity = speed * ((target.transform.position - transform.position).normalized) ;
-			animator.SetBool("walking", true);
-		}
-		else
-		{
-			rb.velocity = Vector2.zero;
-			Attack();
-		}
-		if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
-			facing = (rb.velocity.x > 0 ? 0 : 2);
-		else
-			facing = (rb.velocity.y > 0 ? 1 : 3);
-			
-		if(!attacking)
-			animator.SetInteger("facing", facing);
-	}
-	
-	public void Attack()
-	{
-		attacking = true;
-		if(!type)
-		{
-			animator.SetBool("walking", false);
-			animator.SetInteger("facing", 1);
-		}	
-		else 
-		{
-			animator.SetBool("walking", false);
-			animator.SetInteger("facing", 1);
+			animator.SetBool("rest", false);
+			target.Damage((type ? 1 : 2), this);
+			if(type)
+			{
+				rb.velocity = -3 * speed * ((target.transform.position - transform.position).normalized);
+			}
 		}
 	}
 }

83798395324c26bd6f6468bef273c7fdab0c2bec
David Koloski <djkoloski@gmail.com>
2015-03-01 02:40:11 -0500

    Removed integer GUI, added health GUI, made shots and made the shooter fire shots

--- Assets/Shooter.cs
+++ Assets/Shooter.cs
@@ -6,17 +6,23 @@ public class Shooter : MonoBehaviour
 {
 	private SpriteRenderer sr;
 
+	public GameObject bullet;
 	public float radius;
-	public float converge_speed;
+	public float convergeSpeed;
+	public float shotSpeed;
 	
+	private bool shooting;
 	private float curAngle;
 	private float toAngle;
+	private float shotTimeout;
 
 	public void Awake()
 	{
 		sr = GetComponent<SpriteRenderer>();
+		shooting = false;
 		curAngle = 0;
 		toAngle = 0;
+		shotTimeout = 0;
 	}
 
 	public void Start()
@@ -33,17 +39,33 @@ public class Shooter : MonoBehaviour
 		if (Mathf.Abs(curAngle - toAngle) < Mathf.Abs(curAngle - nearest_angle))
 			nearest_angle = toAngle;
 
-		curAngle = Mathf.Lerp(curAngle, nearest_angle, Time.deltaTime * converge_speed);
+		curAngle = Mathf.Lerp(curAngle, nearest_angle, Time.deltaTime * convergeSpeed);
 		transform.localPosition = new Vector3(Mathf.Cos(curAngle), Mathf.Sin(curAngle), 0.0f) * radius;
 
 		if ((curAngle > 0 && curAngle < Mathf.PI) || curAngle < -Mathf.PI)
 			sr.sortingOrder = -2;
 		else
 			sr.sortingOrder = 0;
+
+		if (shooting && shotTimeout <= 0.0f)
+			Shoot();
+		else
+			shotTimeout -= Time.deltaTime;
+	}
+
+	public void Shoot()
+	{
+		GameObject go = (GameObject)GameObject.Instantiate(bullet);
+		go.transform.position = transform.position + new Vector3(Mathf.Cos(curAngle), Mathf.Sin(curAngle), 0.0f) * 0.2f;
+		Bullet b = go.GetComponent<Bullet>();
+		b.direction = new Vector2(Mathf.Cos(curAngle), Mathf.Sin(curAngle));
+
+		shotTimeout = shotSpeed;
 	}
 
 	public void SetAim(Vector2 direction)
 	{
+		shooting = direction.magnitude > .7;
 		toAngle = Mathf.Atan2(direction.y, direction.x);
 	}
 }
